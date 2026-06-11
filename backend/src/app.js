c53a17856bcaedfc40da4e2074c5f6fe805600ce
Matthew Dsouza <matthewdsouza.one@outlook.com>
2026-06-12 03:05:07 +0530

    feat: setup mongodb atlas connection

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.listen(5000, () => {
+  console.log("Server running");
+});
